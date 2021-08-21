614e7874eca6d7ac08c208b7c1c317d1a691ac69
Benjamin <benjaminrenny@gmail.com>
2021-08-21 11:01:17 +0100

    changed firebase id

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDuAMor44xql4rjgobCzYBzh4Arn0el39Q",
-    authDomain: "linkedin-clone-yt-55da2.firebaseapp.com",
-    projectId: "linkedin-clone-yt-55da2",
-    storageBucket: "linkedin-clone-yt-55da2.appspot.com",
-    messagingSenderId: "315992660619",
-    appId: "1:315992660619:web:bb9aef1a0806e3d42c928e"
+    apiKey: "AIzaSyBSokOFNIFS8C5K0r1k81rhPHRq5HUgoPY",
+    authDomain: "dealr-6bc49.firebaseapp.com",
+    projectId: "dealr-6bc49",
+    storageBucket: "dealr-6bc49.appspot.com",
+    messagingSenderId: "23127227802",
+    appId: "1:23127227802:web:07fe8ead73ebd3fc270e23"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
